c06e6c30115c780f9a9063862adddec72fc7af31
porovoz <falkon1@list.ru>
2024-04-11 12:45:10 +0300

    initial commit

---
+++ src/main/java/com/bestapp/SimpleTaskManagementSystem/SimpleTaskManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bestapp.SimpleTaskManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTaskManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTaskManagementSystemApplication.class, args);
+	}
+
+}
