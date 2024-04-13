229728ca6168dded09425e4c34914df9e9fa7a02
porovoz <falkon1@list.ru>
2024-04-13 15:00:09 +0300

    Logger added

--- src/main/java/com/bestapp/SimpleTaskManagementSystem/service/impl/TaskServiceImpl.java
+++ src/main/java/com/bestapp/SimpleTaskManagementSystem/service/impl/TaskServiceImpl.java
@@ -8,6 +8,7 @@ import com.bestapp.SimpleTaskManagementSystem.repository.TaskRepository;
 import com.bestapp.SimpleTaskManagementSystem.service.TaskMapper;
 import com.bestapp.SimpleTaskManagementSystem.service.TaskService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class TaskServiceImpl implements TaskService {
 
@@ -24,40 +26,52 @@ public class TaskServiceImpl implements TaskService {
     @Override
     @Transactional
     public TaskDTO createTask(CreateOrUpdateTaskDTO createOrUpdateTaskDTO) {
+        log.info("Create task method was invoked");
         Task createdTask = taskRepository.save(taskMapper.toTask(createOrUpdateTaskDTO));
-        return taskMapper.toTaskDTO(createdTask);
+        TaskDTO taskDTO = taskMapper.toTaskDTO(createdTask);
+        log.info("Task {} was created successfully", taskDTO);
+        return taskDTO;
     }
 
     @Override
     @Transactional(readOnly = true)
     public TaskDTO findTaskById(Long id) {
+        log.info("Find task by id = {} method was invoked", id);
         Task foundTask = taskRepository.findById(id).orElseThrow(TaskNotFoundException::new);
+        log.info("Task with id = {} was successfully found", id);
         return taskMapper.toTaskDTO(foundTask);
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<TaskDTO> findAllTasks(Integer pageNumber, Integer pageSize) {
+        log.info("Find all tasks method was invoked");
         if (pageSize > 50 || pageSize <= 0) {
             pageSize = 50;
         }
         PageRequest pageRequest = PageRequest.of(pageNumber - 1, pageSize);
         List<Task> tasks = taskRepository.findAll(pageRequest).getContent();
+        log.info("All tasks were successfully found");
         return taskMapper.toTaskDTOList(tasks);
     }
 
     @Override
     @Transactional
     public TaskDTO updateTask(Long id, CreateOrUpdateTaskDTO createOrUpdateTaskDTO) {
+        log.info("Update task method was invoked");
         Task task = taskRepository.findById(id).orElseThrow(TaskNotFoundException::new);
         taskMapper.updateTask(createOrUpdateTaskDTO, task);
         task = taskRepository.save(task);
-        return taskMapper.toTaskDTO(task);
+        TaskDTO taskDTO = taskMapper.toTaskDTO(task);
+        log.info("Task {} was updated successfully", taskDTO);
+        return taskDTO;
     }
 
     @Override
     @Transactional
     public void deleteTaskById(Long id) {
+        log.info("Delete task by id = {} method was invoked", id);
         taskRepository.deleteById(id);
+        log.info("Task with id = {} was deleted successfully", id);
     }
 }
